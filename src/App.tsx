bf4bdb0d31c71b21e75fa0593d92f87868a385a0
Dawid Dahl <dawiddahl@gmail.com>
2020-01-31 07:18:26 +0100

    fixed css bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import AddInput from "./components/AddInput";
 import {Graybox} from "./components/graybox/Graybox";
+import {useSelector} from "react-redux";
+import {RootState} from "./store";
 
 const App: React.FC = () => {
+	const isAddingTodo = useSelector((state: RootState) => state.reducer.isAddingTodo);
+
 	return (
 		<div className="app">
-			<Graybox />
+			{isAddingTodo ? <Graybox /> : ""}
 			<Header />
 			<TodoList />
 			<AddInput />
--- src/components/AddInput.tsx
+++ src/components/AddInput.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {actionCreator} from "../reducers";
+import {actionCreator} from "../utils/utils";
 import {useDispatch} from "react-redux";
 
 const AddInput = () => {
@@ -18,7 +18,12 @@ const AddInput = () => {
 					d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
 				></path>
 			</svg>
-			<p className="add-input__add-todo-p">Add Todo</p>
+			<p
+				className="add-input__add-todo-p"
+				onClick={() => dispatch(actionCreator("TOGGLE_IS_ADDING_TODO"))}
+			>
+				Add Todo
+			</p>
 		</div>
 	);
 };
